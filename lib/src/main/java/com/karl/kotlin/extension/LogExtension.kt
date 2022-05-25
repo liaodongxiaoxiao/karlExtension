67cc866a50d85e0e07dffb494a59ffdd69515761
wangzhuo <wangzhuo@autoai.com>
2022-05-25 17:42:18 +0800

    优化 log extension

--- lib/src/main/java/com/karl/kotlin/extension/LogExtension.kt
+++ lib/src/main/java/com/karl/kotlin/extension/LogExtension.kt
@@ -8,15 +8,12 @@ const val TAG = "KarlTag"
 inline fun <reified T> T?.log(log: Boolean = true) {
     if (log) {
         val name = T::class.java.simpleName
-        //Log.e(TAG, "$name:${this.toString()}")
         toLog(TAG, "$name:${this.toString()}")
     }
 }
 
 inline fun <reified T> T?.log(key: String, log: Boolean = true) {
     if (log) {
-        //val name = T::class.java.simpleName
-        //Log.e(TAG, "$key--->:${this.toString()}")
         toLog(TAG, "$key--->:${this.toString()}")
     }
 }
@@ -26,15 +23,12 @@ inline fun <reified T> T?.jsonLog(log: Boolean = true) {
     if (log) {
         val gson = Gson()
         val name = T::class.java.simpleName
-        //Log.e(TAG, "$name:${gson.toJson(this)}")
         toLog(TAG, "$name:${gson.toJson(this)}")
     }
 }
 
-fun Throwable.log(tag: String = TAG, log: Boolean = true) {
-    if (log) {
-        Log.e(TAG, this.message, this)
-    }
+fun Throwable.log(tag: String = TAG) {
+    Log.e(TAG, "Exception Info:${this.message}", this)
 }
 
 fun toLog(tag: String, msg: String) {
