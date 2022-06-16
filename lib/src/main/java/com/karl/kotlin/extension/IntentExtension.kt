607fe4be7dd9c07766ef93fdde60d639cf1e3863
wangzhuo <wangzhuo@autoai.com>
2022-06-16 09:25:20 +0800

    add intent is null check

--- lib/src/main/java/com/karl/kotlin/extension/IntentExtension.kt
+++ lib/src/main/java/com/karl/kotlin/extension/IntentExtension.kt
@@ -3,40 +3,40 @@ package com.karl.kotlin.extension
 import android.content.Intent
 import android.os.Parcelable
 
-inline fun Intent.getStrParam(key: String, defaultValue: String = ""): String {
-    return if (this.hasExtra(key)) {
+inline fun Intent?.getStrParam(key: String, defaultValue: String = ""): String {
+    return if (this != null && this.hasExtra(key)) {
         this.getStringExtra(key) ?: defaultValue
     } else {
         defaultValue
     }
 }
 
-inline fun Intent.getIntParam(key: String, defaultValue: Int = 0): Int {
-    return if (this.hasExtra(key)) {
+inline fun Intent?.getIntParam(key: String, defaultValue: Int = 0): Int {
+    return if (this != null && this.hasExtra(key)) {
         this.getIntExtra(key, defaultValue)
     } else {
         defaultValue
     }
 }
 
-inline fun Intent.getDoubleParam(key: String, defaultValue: Double = 0.0): Double {
-    return if (this.hasExtra(key)) {
+inline fun Intent?.getDoubleParam(key: String, defaultValue: Double = 0.0): Double {
+    return if (this != null && this.hasExtra(key)) {
         this.getDoubleExtra(key, defaultValue)
     } else {
         defaultValue
     }
 }
 
-inline fun Intent.getBooleanParam(key: String, defaultValue: Boolean = false): Boolean {
-    return if (this.hasExtra(key)) {
+inline fun Intent?.getBooleanParam(key: String, defaultValue: Boolean = false): Boolean {
+    return if (this != null && this.hasExtra(key)) {
         this.getBooleanExtra(key, defaultValue)
     } else {
         defaultValue
     }
 }
 
-inline fun <reified T : Parcelable> Intent.getParcelableParam(key: String): T? {
-    return if (this.hasExtra(key)) {
+inline fun <reified T : Parcelable> Intent?.getParcelableParam(key: String): T? {
+    return if (this != null && this.hasExtra(key)) {
         this.getParcelableExtra<T>(key)
     } else {
         null
